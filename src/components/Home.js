5685fe46ffcd67253a1efb487a7d91393825ac03
GainorJoshua-FS <JQGainor@student.fullsail.edu>
2021-11-12 12:38:37 -0500

    Home page started, shows 1 random game at the time

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+
+
 
 function Home() {
+
+    const { gid } = useParams()
+    const randomURL = "https://api.boardgameatlas.com/api/search?client_id=lkziTbYVbS&random=true"
+    // const testURL = "https://api.boardgameatlas.com/api/search?client_id=lkziTbYVbS&order_by=rank&ascending=true&limit=4"
+
+    let [randomArray, setRandomArray] = useState([]);
+
+    useEffect(()=>{
+        showGames(gid)
+    },[gid])
+
+    function showGames(gameId){
+
+        fetch(randomURL)
+        .then(response => response.json())
+        .then((data)=>{
+            console.log(data)
+            setRandomArray(data.games)
+        })
+
+    }
+
+    //tried {JSON.stringify(randomArray)} since random only returns one game
+
     return (
         <div>
-            <h1>You Are Home</h1>
+            {randomArray.map((e,i)=>{
+                return <article key={e.id}>
+                    <h2>
+                        {e.name}
+                    </h2>
+                    <img src={e.images.small} alt={e.name + "'s box art"} />
+                </article>
+            })}
         </div>
     )
 }
