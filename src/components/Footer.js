44b702d313f3a1656def864689dc445835044720
GainorJoshua-FS <JQGainor@student.fullsail.edu>
2021-11-18 19:17:49 -0500

    Fixed links in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,10 @@ function Footer() {
     return (
         <footer style={styles.footer}>
             <ul style={styles.links}>
-                <li><Link style={styles.link} to="www.linkedin.com/in/joshua-gainor-0a42a0179">LinkedIn</Link></li>
-                <li><Link style={styles.link} to="https://github.com/GainorJoshua-FS/WDD-Portfolio4">Github</Link></li>
+                {/* <li><Link style={styles.link} to="www.linkedin.com/in/joshua-gainor-0a42a0179">LinkedIn</Link></li> */}
+                {/* <li><Link style={styles.link} to="https://github.com/GainorJoshua-FS/WDD-Portfolio4">Github</Link></li> */}
+                <li><a style={styles.link} href="www.linkedin.com/in/joshua-gainor-0a42a0179" target="_blank" rel="noreferrer" >LinkedIn</a></li>
+                <li><a style={styles.link} href="https://github.com/GainorJoshua-FS/WDD-Portfolio4" target="_blank" rel="noreferrer" >Github</a></li>
             </ul>
             <p>© 2021 BoardDebut. All rights reserved. <a style={styles.link} href="/">Terms and Conditions</a></p>
             <p><Link style={styles.link} to="/">Privacy Policy</Link></p>
